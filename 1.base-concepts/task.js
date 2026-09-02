5dd86fb57d2a1e801ba2e9a48bb788c9a40f59c0
artem <ruviel2015@gmail.com>
2026-09-02 05:55:30 +0500

    Первое задание выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,20 +1,29 @@
 "use strict";
 
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  const d = b ** 2 - 4 * a * c;
+	let arr = [];
+	let d = Math.pow(b, 2) - 4 * a * c;
 
-  if (d === 0) {
-    arr.push(-b / (2 * a));
-  } else if (d > 0) {
-    arr.push((-b + Math.sqrt(d)) / (2 * a));
-    arr.push((-b - Math.sqrt(d)) / (2 * a));
-  }
+	if (d > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a));
+	} else if (d === 0) {
+		arr.push(-b / (2 * a));
+	}
 
-  return arr;
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let S = amount - contribution;
+
+	if (S <= 0) {
+		return 0;
+	}
+
+	let P = (percent / 100) / 12;
+	let monthlyPayment = S * (P + (P / (Math.pow(1 + P, countMonths) - 1)));
+	let totalAmount = monthlyPayment * countMonths;
+
+	return Number(totalAmount.toFixed(2));
 }
