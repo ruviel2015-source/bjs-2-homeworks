83925360e0c0b9fe15807fede68ac13ac5870f9c
artem <ruviel2015@gmail.com>
2026-09-20 05:51:39 +0500

    Шестое задание выполнено

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,50 @@
-﻿
+﻿function parseCount(value) {
+	const result = Number.parseFloat(value);
+	if (Number.isNaN(result)) {
+		throw new Error("Невалидное значение");
+	}
+	return result;
+}
+
+function validateCount(value) {
+	try {
+		return parseCount(value);
+	} catch (error) {
+		return error;
+	}
+}
+class Triangle {
+	constructor(a, b, c) {
+		if (a + b <= c || a + c <= b || b + c <= a) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	get perimeter() {
+		return this.a + this.b + this.c;
+	}
+
+	get area() {
+		const p = this.perimeter / 2;
+		const value = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+		return Number(value.toFixed(3));
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (error) {
+		return {
+			get area() {
+				return "Ошибка! Треугольник не существует";
+			},
+			get perimeter() {
+				return "Ошибка! Треугольник не существует";
+			}
+		};
+	}
+}
